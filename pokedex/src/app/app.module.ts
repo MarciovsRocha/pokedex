d8d45c1367c8eb2f5e341caf4e18aa3a3f067119
Marcio Vinicius de Souza da Rocha <dev.marcio.rocha@gmail.com>
2023-02-23 22:48:52 -0300

    importados components criados

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -4,10 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { CardComponent } from './components/card/card.component';
+import { FrontcardsComponent } from './paginas/frontcards/frontcards.component';
+import { BackcardsComponent } from './paginas/backcards/backcards.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    FrontcardsComponent,
+    BackcardsComponent
   ],
   imports: [
     BrowserModule,
