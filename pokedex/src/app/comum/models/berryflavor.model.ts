54245ebf5065759c9687a796c4456bb45d9682e4
Marcio Vinicius de Souza da Rocha <dev.marcio.rocha@gmail.com>
2023-02-25 14:00:16 -0300

    criadas classes para endpoint BerryFlavors

---
+++ pokedex/src/app/comum/models/berryflavor.model.ts
@@ -0,0 +1,15 @@
+import { Name } from "./name.model";
+import { NamedAPIResource } from "./namedapiresource.model";
+import { FlavorBerryMap } from "./flavorberrymap.model";
+
+// endpoint {base_url}/berry-flavor/{id or name}
+// type
+export class BerryFlavor{
+  constructor(
+  public id?: number
+  , public name?: string
+  , public berries?: FlavorBerryMap[]
+  , public contest_type?: NamedAPIResource // ContestType
+  , public names?: Name[]
+  ){}
+}
---
+++ pokedex/src/app/comum/models/flavorberrymap.model.ts
@@ -0,0 +1,9 @@
+import { NamedAPIResource } from "./namedapiresource.model";
+
+// type
+export class FlavorBerryMap{
+  constructor(
+    public potency?: number
+    , public berry?: NamedAPIResource // berry
+  ){}
+}
