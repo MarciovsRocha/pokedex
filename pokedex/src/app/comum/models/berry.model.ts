2e2712fe22b9884729743ea259b0bf1cc7454156
Marcio Vinicius de Souza da Rocha <dev.marcio.rocha@gmail.com>
2023-02-25 13:56:35 -0300

    criadas classes para mapeamento de berry

---
+++ pokedex/src/app/comum/models/berry.model.ts
@@ -0,0 +1,21 @@
+import { BerryFlavorMap } from "./berryflavormap.model";
+import { NamedAPIResource } from "./namedapiresource.model";
+
+// endpoint {base_url}/berry/{id or name}
+// type
+export class Berry{
+  constructor(
+    public id?: number
+    , public name?: string
+    , public growth_time?: number
+    , public max_harvest?: number
+    , public natural_gift_power?: number
+    , public size?: number
+    , public smoothness?: number
+    , public soil_druness?: number
+    , public firmness?: NamedAPIResource
+    , public flavors?: BerryFlavorMap[]
+    , public item?: NamedAPIResource // Item
+    , public natural_gift_type?: NamedAPIResource // Type
+  ){}
+}
