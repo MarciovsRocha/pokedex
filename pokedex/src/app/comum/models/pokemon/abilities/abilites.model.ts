a3e25d6ae3f12d4fd900a2bc33108d932c7e5ffb
Marcio Vinicius de Souza da Rocha <dev.marcio.rocha@gmail.com>
2023-02-26 16:03:06 -0300

    finalizado desenvolvimento do endpoint Abilities

--- pokedex/src/app/comum/models/pokemon/abilities/abilites.model.ts
+++ pokedex/src/app/comum/models/pokemon/abilities/abilites.model.ts
@@ -1,4 +1,7 @@
-import { NamedAPIResource } from "../../utility";
+import { Name, NamedAPIResource, VerboseEffect } from "../../utility";
+import { AbilityEffectChange } from "./abilityeffectchange.model";
+import { AbilityFlavorText } from "./abilityflavortext.model";
+import { AbilityPokemon } from "./abilitypokemon.model";
 
 // endpoint {base_url}/ability/{id or name}
 // type
@@ -7,7 +10,11 @@ export class Abilities{
     public id?: number
     , public name?: string
     , public is_main_series?: boolean
-    , public generation?: NamedAPIResource
-    ,
+    , public generation?: NamedAPIResource // Generation
+    , public names?: Name[]
+    , public effect_entries?: VerboseEffect[]
+    , public effect_changes?: AbilityEffectChange[]
+    , public flavor_text_entries?: AbilityFlavorText[]
+    , public pokemon?: AbilityPokemon[]
   ){}
 }
